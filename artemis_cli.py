08311ee025a7632b316b723a779b6029ab71ec76
Lukas Gerhardt <lukas.gerhardt@tum.de>
2019-11-08 17:52:28 +0100

    Add arguments

--- artemis_cli.py
+++ artemis_cli.py
@@ -6,9 +6,18 @@ import argparse
 
 from detail.artemis_api import ArtemisAPI
 
+
 parser = argparse.ArgumentParser(description='A command-line application for tutors to more productively grade programming excises on ArTEMiS')
 # TODO: think of and parse arguments
-args = parser.parse_args();
+parser.add_argument('command', help='The command for the script. Valid commands are "downloadrepos", "getscores", "newresult".')
+parser.add_argument('-s', '--students', metavar='', nargs='+', help='The students TUM ids to be processed (e.g. ge36feg ba12sup, ...)')
+parser.add_argument('-a', '--assignment', metavar='', nargs=1, help='The assignment to be processed (e.g. w01h01)')
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-q', '--quiet', action='store_true', help='Print quiet')
+group.add_argument('-v', '--verbose', action='store_true', help='Print verbose')
+
+args = parser.parse_args()
 
 with open('config.yml', 'r') as config_file:
     cfg = yaml.safe_load(config_file)
