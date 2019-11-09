0dab321a0e2ee5d68642a443d992fc308c3f43d2
Lukas Gerhardt <lukas.gerhardt@tum.de>
2019-11-09 11:59:45 +0100

    Implement nargs='+' (one or more) for -pos and -neg arguments in new result command, PEP8 style and indentation fixes

--- detail/arg_parser.py
+++ detail/arg_parser.py
@@ -1,9 +1,11 @@
 import argparse
 
+
 class ArgParser(argparse.ArgumentParser):
     def __init__(self):
         super(ArgParser, self).__init__(
-            description='A command-line application for tutors to more productively grade programming excises on ArTEMiS')
+            description='A command-line application for tutors to more productively '
+                        'grade programming excises on ArTEMiS')
 
         sub_parsers = self.add_subparsers(
             title='commands',
@@ -13,30 +15,28 @@ class ArgParser(argparse.ArgumentParser):
             parser_class=argparse.ArgumentParser)
 
         repos_parser = sub_parsers.add_parser('repos',
-            help='Download student exercise repositories')
+                                              help='Download student exercise repositories')
 
         repos_parser.add_argument('-a', '--assignment',
-            metavar='assignment',
-            nargs=None,
-            help='The assignment to be processed (e.g. w01h01)')
+                                  metavar='assignment',
+                                  help='The assignment to be processed (e.g. w01h01)')
 
         repos_parser.add_argument('-s', '--students',
-            metavar='tumId',
-            nargs='+',
-            help='The students TUM ids to be processed (e.g. ge36feg ba12sup, ...)')
+                                  metavar='tumId',
+                                  nargs='+',
+                                  help='The students TUM ids to be processed (e.g. ge36feg ba12sup, ...)')
 
         scores_parser = sub_parsers.add_parser('scores',
-            help='Get scores for students\' assignments [not yet implemented]')
+                                               help='Get scores for students\' assignments [not yet implemented]')
 
         scores_parser.add_argument('-a', '--assignment',
-            metavar='assignment',
-            nargs=None,
-            help='The assignment to be processed (e.g. w01h01)')
+                                   metavar='assignment',
+                                   help='The assignment to be processed (e.g. w01h01)')
 
         scores_parser.add_argument('-s', '--students',
-            metavar='tumId',
-            nargs='+',
-            help='The students TUM ids to be processed (e.g. ge36feg ba12sup, ...)')
+                                   metavar='tumId',
+                                   nargs='+',
+                                   help='The students TUM ids to be processed (e.g. ge36feg ba12sup, ...)')
 
         # newresult
         # sytax:
@@ -45,44 +45,43 @@ class ArgParser(argparse.ArgumentParser):
         #   -positive "Kommentare" "Gute Dokumentation"
         #   -negative "Formatierung" "Bitte nutze Autoformat"
         result_parser = sub_parsers.add_parser('newresult',
-            help='Post a new result for a student\'s assignment [not yet implemented]')
+                                               help='Post a new result for a student\'s assignment '
+                                                    '[not yet implemented]')
 
         result_parser.add_argument('-a', '--assignment',
-            metavar='assignment',
-            required=True,
-            nargs=None,
-            help='The assignment to be processed (e.g. w01h01)')
+                                   metavar='assignment',
+                                   required=True,
+                                   help='The assignment to be processed (e.g. w01h01)')
 
         result_parser.add_argument('-s', '--student',
-            required=True,
-            metavar='tum_id',
-            nargs=None,
-            help='The students TUM id to be processed (e.g. ge36feg)')
+                                   required=True,
+                                   metavar='tum_id',
+                                   help='The students TUM id to be processed (e.g. ge36feg)')
 
         result_parser.add_argument('-score',
-            metavar='score',
-            required=True,
-            type=int,
-            nargs=None,
-            help='The Score of the assignment (e.g. 80)')
+                                   metavar='score',
+                                   required=True,
+                                   type=int,
+                                   help='The Score of the assignment (e.g. 80)')
 
         result_parser.add_argument('-text',
-            required=True,
-            metavar='result_text',
-            nargs=None,
-            help='The Result Text of the assignment (e.g. "Gut gemacht")')
+                                   required=True,
+                                   metavar='result_text',
+                                   help='The Result Text of the assignment (e.g. "Gut gemacht")')
 
         result_parser.add_argument('-pos', '--positive',
-           metavar=('text', 'detail_text'),
-           nargs=2,
-           action='append',
-           help='A positive feedback consisting of Text and Detail Text (e.g. "Dokumentation" "Gute und akkurate Kommentare")')
+                                   metavar=('text', 'detail_text'),
+                                   nargs='+',  # at least one
+                                   action='append',
+                                   help='A positive feedback consisting of Text and Detail Text '
+                                        '(e.g. "Dokumentation" "Gute und akkurate Kommentare")')
 
         result_parser.add_argument('-neg', '--negative',
-           metavar=('text', 'detail_text'),
-           nargs=2,
-           action='append',
-           help='A negative feedback consisting of Text and Detail Text (e.g."Formatierung" "Bitte Autoformatierung benutzen")')
+                                   metavar=('text', 'detail_text'),
+                                   nargs='+',  # at least one
+                                   action='append',
+                                   help='A negative feedback consisting of Text and Detail Text '
+                                        '(e.g."Formatierung" "Bitte Autoformatierung benutzen")')
 
         # allows only one of the specified arguments
         group = self.add_mutually_exclusive_group()
